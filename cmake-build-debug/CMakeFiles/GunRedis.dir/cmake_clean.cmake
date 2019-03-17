bf7683ec2556e113010e43acaac7981aec756ba1
dosdrtt <lnpj.goverment@outlook.com>
2019-03-17 18:14:52 +0800

    first update

---
+++ cmake-build-debug/CMakeFiles/GunRedis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GunRedis.dir/main.cpp.o"
+  "GunRedis.pdb"
+  "GunRedis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GunRedis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GunRedis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GunRedis.
+# This may be replaced when dependencies are built.
